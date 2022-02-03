e64b8971e637f77fcf272fea3e6a8e1fae8ff31d
Ryan Walker <rwalker@nexient.com>
2022-02-03 00:38:15 -0500

    svg rendering

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import mapSvg from "../data/demo_map.svg";
-// import { ReactComponent as MapSVG } from "../data/demo_map.svg";
+import { ReactComponent as MapSVG } from "../data/demo_map.svg";
 
 const Map = () => {
   return (
     <div>
-      <img src={mapSvg} alt="map" />
-      {/* <MapSVG /> */}
+      {/* <img src={mapSvg} alt="map" /> */}
+      <MapSVG />
     </div>
   );
 };
