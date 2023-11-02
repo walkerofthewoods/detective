8a85b7120c5fbcdf0c608802a06a2c78a9a68b25
Ryan Walker <walkerrm@gmail.com>
2023-11-02 10:30:43 -0400

    update file structure

--- src/components/directory.tsx
+++ src/components/directory.tsx
@@ -1,7 +1,7 @@
-import { Button } from "../../@/components/ui/button";
-import { Input } from "../../@/components/ui/input";
-import { Label } from "../../@/components/ui/label";
-import { RadioGroup, RadioGroupItem } from "../../@/components/ui/radio-group";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 import { useState } from "react";
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import tailwindcss from "tailwindcss";
 import { TailwindCSSVitePlugin } from "tailwindcss-vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), TailwindCSSVitePlugin()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   css: {
     postcss: {
       plugins: [tailwindcss],
